d7cb0b216d66b5af35474dd9a52622b95c9ce839
giannis <john_z94@hotmail.com>
2020-11-14 23:55:36 +0200

    refactor: game status bar in a new component

--- src/web/src/components/GameScreen.vue
+++ src/web/src/components/GameScreen.vue
@@ -1,18 +1,6 @@
 <template>
   <div class="board container">
-    <div id= "turnRow" class="row" style="background-color: black;min-height: 80px">
-        <div id="titleDiv" class="col-md-3" style="color: lightblue;">Othello Riversi</div>
-        <div id="turnDiv" class="col-md-3 text-center" v-show="!showOptions">{{ player }} player</div>
-        <div id="scoreDiv" class="col-md-3 text-center" v-show="!showOptions" style="color: white">
-          <i class="fas fa-circle" style="borderColor: white; border-radius:20px; border-style:solid; color: black"/> {{ getScore[0] }}
-          <i class="fas fa-circle" style="borderColor: white; border-radius:20px; border-style:solid; color: white"/> {{ getScore[1] }}
-        </div>
-        <div id="resetDiv" class="col-md-3 text-center" v-show="!showOptions">
-          <a @click.prevent="reset()" class="reset">
-            New Game <i class="fas fa-redo"></i>
-          </a>
-        </div>
-    </div>
+    <GameStatusBar :score-info="getScore" :player="player" :render-game-info="showOptions" @new-game="reset"/>
     <Options v-if="showOptions" :options="options" @select="choose" @close="choose" />
     <template v-else>
       <GameBoard :gameId="gameId" :board="board" @played="onPlayed"/>
@@ -25,10 +13,11 @@
   import Options from '@/components/modals/ChooseColor';
   import GameBoard from '@/components/GameBoard';
   import TextUI from '@/components/TextUI';
+  import GameStatusBar from '@/components/GameStatusBar';
 
   export default {
     name: 'GameScreen',
-    components: { GameBoard, Options, TextUI },
+    components: { GameBoard, GameStatusBar, Options, TextUI },
     data() {
       return {
         msgs: [],
@@ -55,6 +44,7 @@
         this.axios.get(`/api/game/choose`, { params: { playerColor: color || this.options[0], gameId: this.gameId }}).then((response) => {
           this.showOptions = false;
           this.msgs.push(response.data.gameMessage);
+          // TODO handle AI move if user choose WHITE
         })
       },
       init() {
@@ -84,48 +74,9 @@
 </script>
 
 <style scoped>
-   #turnDiv{
-      margin-left: 3%;
-      background-color: green;
-      color: white;
-      min-height: 30px;
-      width: 150px;
-      padding: 10px;
-      margin-top: 1%;
-      border-radius: 7%;
-      font-size: 150%;
-   }
-
-   #scoreDiv{
-      margin-left: 3%;
-      margin-top: 1%;
-      font-size: 200%;
-   }
-
-   #resetDiv{
-      margin-left: 3%;
-      margin-top: 1%;
-      font-size: 200%;
-   }
-
-   #titleDiv{
-      margin-top: 1%;
-      font-size: 250%;
-   }
-
-   #seconds{
-      color: white;
-   }
-
   .board {
     width:100%;
     background-color: gray;
     min-height: 800px
-  }
-
-  .reset {
-    color: red;
-    text-decoration: none;
-    cursor: pointer;
   }
 </style>
---
+++ src/web/src/components/GameStatusBar.vue
@@ -0,0 +1,69 @@
+<template>
+  <div id= "turnRow" class="row">
+      <div id="titleDiv" class="col-md-3" style="color: lightblue">Othello Riversi</div>
+      <div id="turnDiv" class="col-md-3 text-center" v-show="!renderGameInfo">{{ player }} player</div>
+      <div id="scoreDiv" class="col-md-3 text-center" v-show="!renderGameInfo" style="color: white">
+        <i class="fas fa-circle" style="borderColor: white; border-radius:20px; border-style:solid; color: black"/> {{ scoreInfo[0] }}
+        <i class="fas fa-circle" style="borderColor: white; border-radius:20px; border-style:solid; color: white"/> {{ scoreInfo[1] }}
+      </div>
+      <div id="resetDiv" class="col-md-3 text-center" v-show="!renderGameInfo">
+        <a @click.prevent="reset()" class="reset">
+          New Game <i class="fas fa-redo"></i>
+        </a>
+      </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'GameStatusBar',
+    props: ['scoreInfo', 'player', 'renderGameInfo'],
+    methods: {
+      reset() {
+        this.$emit('new-game');
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  #turnRow{
+    background-color: black;
+    min-height: 80px;
+  }
+
+  #turnDiv{
+     margin-left: 3%;
+     background-color: green;
+     color: white;
+     min-height: 30px;
+     width: 150px;
+     padding: 10px;
+     margin-top: 1%;
+     border-radius: 7%;
+     font-size: 150%;
+  }
+
+  #scoreDiv{
+     margin-left: 3%;
+     margin-top: 1%;
+     font-size: 200%;
+  }
+
+  #resetDiv{
+     margin-left: 3%;
+     margin-top: 1%;
+     font-size: 200%;
+  }
+
+  #titleDiv{
+     margin-top: 1%;
+     font-size: 250%;
+  }
+
+ .reset {
+   color: red;
+   text-decoration: none;
+   cursor: pointer;
+ }
+</style>
